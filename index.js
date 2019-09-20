cf086b2b8df29ee874e5d338ae1df9d9f03bbbca
Kate Park <rudhi1009@gmail.com>
2019-09-19 23:29:27 -0700

    add typescript and gitignore

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// const name = "kate",
-//     age = 10,
-//     gender = "male";
-exports.__esModule = true;
-var sayHi = function (name, age, gender) {
-    console.log("hello, " + name + ", you are " + age + " years old, " + gender);
-};
-sayHi("kate", 22, "female");
